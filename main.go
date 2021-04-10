7d0f7846ad6279606b185512866b039721a4b3b8
Bryan Anderson <shadow431@gmail.com>
2021-04-09 22:31:22 -0700

    Added ToDo's through out. Added destDir argument added lines to remove trailing slashes from the source and dest dirs compaire the source and destination full paths.  If they are the same, don't do anything (this is good for verifing that existing files are where they belong/the script calculates the directory structure correctly)
    added new functions:
    
    getExifInfo = gets all exif metadata about a file
    setDestPath = uses Model, and DateTimeOriginal to calculate where to put the file under destDir
    mvMeida = work through how to move the file
    mvFile = If the file exists check if source and dest are the same using sha256 hash, if no the same, add the serialNumber of the camera to the filename and try again
    getParentMode = walk down the dest directory sROAQture tell the end and get the lowest level's permissions
    makeDir = create remaining folder struction in the destination using permissions from getParentMode
    getHash = generate a sha256 has of a file

--- main.go
+++ main.go
@@ -1,32 +1,59 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"errors"
 	"flag"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"os"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/barasher/go-exiftool"
 )
 
+//TODO
+/*
+  Create a File struct?
+    fileName
+    sourceDir
+	destDir
+	make
+	model
+	serial
+	dateTime
+
+	generate struck for files in setDestPath.
+	rename to setDestPath to procImage?
+	Use struck all the way through then I'll have all the fields I want tell the end
+
+*/
+
 func main() {
 	sourceDir := flag.String("sourceDir", "/tmp", "where to read the images from")
-	//destDir := flag.String("destDir", "/tmp", "where to read the images to")
+	destDir := flag.String("destDir", "/tmp", "where to read the images to")
 	var files []string
 	flag.Parse()
+	*sourceDir = strings.TrimSuffix(*sourceDir, "/")
+	*destDir = strings.TrimSuffix(*destDir, "/")
 
 	e := filepath.Walk(*sourceDir, procDir(&files))
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	//et, err := exiftool.NewExiftool(setExifArgs())
-	et, err := exiftool.NewExiftool(func(s *exiftool.Exiftool) error {
-		s.extraInitArgs = append(s.extraInitArgs, "-ee")
-		return nil
-	})
+	et, err := exiftool.NewExiftool()
+	/*
+		et, err := exiftool.NewExiftool(func(s *exiftool.Exiftool) error {
+			s.extraInitArgs = append(s.extraInitArgs, "-ee")
+			return nil
+		})
+	*/
 	if err != nil {
 		fmt.Printf("Error initializing %v\n", err)
 	}
@@ -35,20 +62,48 @@ func main() {
 	fmt.Printf("%v files Total\n", fileCount)
 
 	for _, file := range files {
-		getExifInfo(et, file)
-		//fmt.Printf("%v\n", file)
+
+		metaData := getExifInfo(et, file)
+		imgPath := setDestPath(metaData)
+		fileSplit := strings.Split(file, "/")
+		filename := fileSplit[len(fileSplit)-1]
+
+		destPath := *destDir + imgPath
+		fullDstPath := destPath + filename
+
+		if file != fullDstPath {
+			fmt.Printf("%v ==> %v\n", file, fullDstPath)
+			mvErr := mvMedia(file, destPath, filename)
+			if mvErr != nil {
+				fmt.Println(mvErr)
+				os.Exit(1)
+			}
+		}
 	}
 
 	defer et.Close()
 
 }
 
+//TODO: Make this works
 /*
 func setExifArgs() exiftool.Exiftool {
 	return func(e *exiftool.Exiftool) {
 		e.extraInitArgs := []string{"-ee"}
 	}
 }*/
+
+//TODO:  what if those fields dont exist?
+func setDestPath(metaData map[string]interface{}) string {
+	var newPath string
+	strDateTime := fmt.Sprintf("%v", metaData["DateTimeOriginal"])
+	arrDate := strings.Fields(strDateTime)
+	newPath = fmt.Sprintf("/%v/", metaData["Model"])
+	newPath += strings.ReplaceAll(arrDate[0], ":", "/")
+	newPath += "/"
+
+	return newPath
+}
 
 func procDir(files *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
@@ -63,7 +118,9 @@ func procDir(files *[]string) filepath.WalkFunc {
 	}
 }
 
-func getExifInfo(et *exiftool.Exiftool, file string) {
+func getExifInfo(et *exiftool.Exiftool, file string) map[string]interface{} {
+	var fields map[string]interface{}
+	//fmt.Printf("%v", file)
 
 	image := et.ExtractMetadata(file)
 
@@ -71,13 +128,129 @@ func getExifInfo(et *exiftool.Exiftool, file string) {
 		if fileInfo.Err != nil {
 			fmt.Printf("Error concerinting %v: %v\n", fileInfo.File, fileInfo.Err)
 		}
-		//if fileInfo.Fields["Make"] == nil || fileInfo.Fields["Model"] == nil || fileInfo.Fields["DateTimeOriginal"] == nil {
-		fmt.Println(file)
-		for field, value := range fileInfo.Fields {
-			fmt.Printf("%v: %v\n", field, value)
-		}
-		//}
+		fields = fileInfo.Fields
 
 	}
+	return fields
+
+}
+
+func mvMedia(source string, dest string, filename string) error {
+	var dirMode os.FileMode
+	destSplit := strings.Split(dest, "/")
+	fullPath := dest + filename
+	var di fs.FileInfo
+	var err error
+
+	dirMode, err = getParentMode(destSplit)
+	if err != nil {
+		return err
+	}
+	di, err = makeDir(dest, dirMode)
+	if err != nil {
+		return err
+	}
+	switch mode := di.Mode(); {
+	case mode.IsDir():
+		err := mvFile(source, fullPath)
+		if err != nil {
+			return err
+		}
+	case mode.IsRegular():
+		fmt.Printf("%v is a file.... WTF!!!!", dest)
+		return err
+	}
+
+	return nil
+}
+
+//WIP
+func mvFile(source string, fullPath string) error {
+	var serialAdded bool
+
+	moved := false
+	for !moved {
+		if _, err := os.Stat(fullPath); errors.Is(err, fs.ErrNotExist) {
+			os.Rename(source, fullPath)
+			moved = true
+		} else {
+			srcHash := getHash(source)
+			dstHash := getHash(fullPath)
+			if bytes.Equal(srcHash, dstHash) && !serialAdded {
+				fmt.Printf("Both files are the same")
+				break
+			}
+			if serialAdded {
+				fmt.Printf("FUCK!!! the files match even after adding the serial")
+			}
+			//Get Cam serial Number, add to start of file name
+			//set Serial as added
+
+		}
+
+	}
+	//TODO:
+	/*
+		         - var serialAdded boolean
+			     - while not created:
+				 -   if !exists:
+				 -     os.Rename()
+				 -   else:
+				 -     getHashes:
+				 -	    if Hash == Hash && !serialAdded:
+				 - 	      dont care they are the same
+				 -  	  break
+						if serialAdded:
+						  FUCK
+					  getCamSerial:
+					    add serial To file Name
+						serialAdded = true
+	*/
+
+	return nil
+}
+
+func getHash(file string) []byte {
+	hasher := sha256.New()
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	defer f.Close()
+	if _, err := io.Copy(hasher, f); err != nil {
+		fmt.Printf("%v\n", err)
+	}
+	return hasher.Sum(nil)
+}
+
+func makeDir(dest string, mode fs.FileMode) (fs.FileInfo, error) {
+	var di fs.FileInfo
+	var err error
+	for di == nil {
+		di, err = os.Stat(dest)
+		if err != nil {
+			err := os.MkdirAll(dest, mode)
+			if err != nil {
+				return di, err
+			}
+
+		}
+	}
+	return di, err
+}
+
+func getParentMode(destSlice []string) (fs.FileMode, error) {
+	forPath := "/"
+	var dirMode os.FileMode
+	var err error
+	for _, path := range destSlice {
+		forPath += path + "/"
+		fi, err := os.Stat(forPath)
+		if err != nil {
+			break
+		}
+		dirMode = fi.Mode()
+	}
+	return dirMode, err
 
 }
